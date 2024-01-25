ce424d310cd344788ba5f44f99d7cbac6e18907c
Anna18FR <147483155+Anna18FR@users.noreply.github.com>
2024-01-25 21:55:55 +0300

    Update script_9.js

--- dz_js_2/js/script_9.js
+++ dz_js_2/js/script_9.js
@@ -4,20 +4,20 @@
 // недели, вывести сообщение «Меньше года», «Меньше месяца» и «Меньше недели», 
 // соответственно. 
 
-// let days=prompt('введите количество дней', '');
-// let years = days / 365;
-// let months = days / 31;
-// let weeks = days / 7;
-// let hour = days / 60;
-// let minutes = hour * 60;
-// let sec = minutes * 60;
-// console.log('Years:', years);
-// console.log('Months:', months);
-// console.log('Weeks:', weeks);
-// console.log('Hour:', hour);
-// console.log('Minutes:', minutes);
-// console.log('Sec:', sec);
+ let days=prompt('введите количество дней', '');
+ let years = days / 365;
+ let months = days / 31;
+ let weeks = days / 7;
+ let hour = days / 60;
+ let minutes = hour * 60;
+ let sec = minutes * 60;
+ console.log('Years:', years);
+ console.log('Months:', months);
+ console.log('Weeks:', weeks);
+ console.log('Hour:', hour);
+ console.log('Minutes:', minutes);
+ console.log('Sec:', sec);
 
-// if (days < 361) {console.log('Меньше года')};
-// if (months < 31) {console.log('Меньше месяца')};
-// if (weeks < 7) {console.log('Меньше недели')};
+ if (days < 361) {console.log('Меньше года')};
+ if (months < 31) {console.log('Меньше месяца')};
+ if (weeks < 7) {console.log('Меньше недели')};
